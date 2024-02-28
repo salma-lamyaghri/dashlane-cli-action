18c83ac0ebbd529acd4957830da96fb4ed3a0779
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-28 16:34:04 +0100

    add access to dcli

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
 curl -0 https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
+mv dcli-linux-x64 dcli 
+chmod +x ./dcli
--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ env_variables=$(printenv | sed 's;=.*;;' | sort)
 for path in $env_variables; do
     # Check if the value of the variable starts with "dl://"
     if [[ "${!path}" =~ dl://* ]]; then
-        echo "$path=$(dcli read ${!path})" >> "$GITHUB_OUTPUT";
+        echo "$path=$(./dcli read ${!path})" >> "$GITHUB_OUTPUT";
     fi;
 done
