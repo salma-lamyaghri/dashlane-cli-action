38e80a31ba763959dc07b0dc1825ff4cee71a266
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-28 21:34:23 +0100

    fix reading github action

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "ACTION_SECRET -> $1";
-
+echo "ACTION_SECRET -> $ACTION_SECRET";
+echo "value 1-> ${!1}";
+echo "value 1-> ${!2}";
 assert_secret() {
   if [ "$(printenv $1)" != "$2" ]; then
     echo -e "Expected $1 to be set to:\n$2\nBut got:\n$(printenv $1)"
