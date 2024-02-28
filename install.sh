a6b3b3105a5f6da182bc5ffc9f5c5f10675d92e9
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-28 16:43:55 +0100

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-curl -o ~/dcli-linux-x64  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
-mv dcli-linux-x64 dcli 
+curl -sSfLo dcli  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
 chmod +x ./dcli
