8a86b6efacc004cd86d332800e2ffde85031d87b
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-28 16:37:46 +0100

    add access to scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 curl -0 https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
 mv dcli-linux-x64 dcli 
 chmod +x ./dcli
--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 env_variables=$(printenv | sed 's;=.*;;' | sort)
 for path in $env_variables; do
     # Check if the value of the variable starts with "dl://"
