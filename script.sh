0f04c8a716f577e3b9713e44dfd3fca21f2b3859
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-28 21:43:53 +0100

    fix reading github action

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ env_variables=$(printenv | sed 's;=.*;;' | sort)
 for path in $env_variables; do
     # Check if the value of the variable starts with "dl://"
     if [[ "${!path}" =~ dl://* ]]; then
-        echo "dcli read ${!path}";
+        #echo "dcli read ${!path}";
         ./dcli sync;
         # ./dcli p -o json;
         # echo "reading password";
