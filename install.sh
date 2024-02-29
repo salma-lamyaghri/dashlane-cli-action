a5616a841468a3a572740b37689c960cdeb6a845
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-29 13:33:03 +0100

    add check for architecture before install :

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
+if [[ "$ARCH" != "amd64" ]] && [[ "$ARCH" != "arm64" ]]; then
+  echo "Unsupported architecture for the Dashlane CLI: $ARCH."
+  exit 1
+fi
+
 curl -sSfLo dcli  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
 chmod +x ./dcli
