7f6b62b02d10df4143d2ac40cffdf1cdc74fe7f9
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-02-29 15:20:21 +0100

    fix checking supported architecture before install

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -e
 
-if [[ "$ARCH" != "amd64" ]] && [[ "$ARCH" != "arm64" ]]; then
-  echo "Unsupported architecture for the Dashlane CLI: $ARCH."
-  exit 1
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Get runner's architecture
+    ARCH=$(uname -m)
+
+    if [[ "$ARCH" != "x86_64" ]] && [[ "$ARCH" != "aarch64" ]]; then
+        echo "Unsupported architecture for the Dashlane CLI: $ARCH."
+        exit 1
+    fi
+
+    curl -sSfLo dcli  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl -sSfLo dcli  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-macos-arm64
+else
+    echo "Operating system not supported yet for this GitHub Action: $OSTYPE."
+    exit 1
 fi
 
-curl -sSfLo dcli  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
 chmod +x ./dcli
