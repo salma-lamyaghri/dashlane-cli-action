cb59109429d58f06f3744c9691ee6359bc0a4c60
salma lamyaghri <salma.lamyaghri@dashlane.com>
2024-03-01 11:22:17 +0100

    try to install in same script

--- install.sh
+++ install.sh
@@ -28,3 +28,4 @@ echo "Installing Dashlane cli ..on OS $OSTYPE."
 # chmod +x ./dcli
 
 curl -sSfLo dcli  https://github.com/Dashlane/dashlane-cli/releases/download/v6.2405.0/dcli-linux-x64
+chmod +x ./dcli
